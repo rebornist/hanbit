3f7fc4ca2a795dc1412f747a101e2b5d539ea303
unknown <rebornist@naver.com>
2020-01-15 12:13:13 +0900

    parcel with wailwindcss 설정 및 base.html 생성 완료

---
+++ static/parcel/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      height: {
+        "25vh": "25vh",
+        "50vh": "50vh",
+        "75vh": "75vh"
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
